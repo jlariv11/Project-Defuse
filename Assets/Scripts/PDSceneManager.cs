2194966b3baf0f695ba3d87c27f5b4e9c72a83c2
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2023-02-22 14:39:56 -0500

    Fixed Metal Border and Started UI Management
    Fixed issues in the main menu, fixed metal border and had it scale correctly depending on screen size.

--- Assets/Scripts/PDSceneManager.cs
+++ Assets/Scripts/PDSceneManager.cs
@@ -5,24 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class PDSceneManager : MonoBehaviour
 {
-    
-    void Awake()
-    {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("SceneManager");
-
-        if (objs.Length > 1)
-        {
-            Destroy(this.gameObject);
-        }
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    public void loadScene(string scene)
+ 
+    public static void loadScene(string scene)
     {
         SceneManager.LoadScene(scene, LoadSceneMode.Single);
     }
 
-    public void exitGame()
+    public static void exitGame()
     {
         Application.Quit();
     }
